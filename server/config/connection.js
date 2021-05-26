b274e745c6f294976844cc3276d2b99f61a82103
MWPX777 <mwpx777@gmail.com>
2021-05-26 09:42:45 -0500

    added concurently

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,5 +1,5 @@
  const mongoose = require('mongoose');
-console.log(process.env.MONGODB_URI);
+console.log( "this is " + process.env.MONGODB_URI);
  mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/webdevgifts',{
      useNewUrlParser: true,
      useUnifiedTopology: true,
