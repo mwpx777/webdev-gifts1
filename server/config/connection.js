501fe4a521c4b25f39f2307412ad2a188dba25d4
MWPX777 <mwpx777@gmail.com>
2021-05-26 07:38:12 -0500

    adjusted connection.js

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,10 +1,10 @@
  const mongoose = require('mongoose');
-
+console.log(process.env.MONGODB_URI);
  mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/webdevgifts',{
      useNewUrlParser: true,
      useUnifiedTopology: true,
      useCreateIndex: true,
      useFindAndModify: false
- });
+ }). catch(error => console.log(error));
 
  module.exports = mongoose.connection;
