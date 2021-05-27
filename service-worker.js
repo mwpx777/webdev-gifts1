7b41ff35860b1274f6b49e6f32e327b3eb66c9eb
MWPX777 <mwpx777@gmail.com>
2021-05-27 10:09:57 -0500

    changed = to +

--- service-worker.js
+++ service-worker.js
@@ -26,7 +26,7 @@ self.addEventListener('activate', function(e) {
 
             return Promise.all(keyList.map(function(key, i){
                 if(cacheKeepList.indexOf ===-1){
-                    console.log('deleting cache: ' = keyList[i]);
+                    console.log('deleting cache: ' + keyList[i]);
                     return caches.delete(keyList[i]);
                 }
             }))
